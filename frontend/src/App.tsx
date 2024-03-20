113d730a6f69b17c3df01239e4a4120d51fa3669
adriroca20 <hardhe21@gmail.com>
2024-03-20 18:33:08 +0100

    Revert "added tailwind to frontend"
    This reverts commit 9f97fff170dfff6aa7b4b5a7d6229469fbb35acc.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,10 @@
 import './App.css'
-import { Header } from './components/Header.tsx'
-import { RadioGroup, RadioGroupItem } from './components/ui/radio-group.tsx'
 
-const sizes = ['xs', 'sm', 'md', 'lg', 'xl']
 
 function App() {
   return (
     <>
-      <Header />
-      <main className='flex justify-center items-center h-screen gap-5 flex-col bg-zinc-950'>
-        <RadioGroup className="flex flex-wrap mb-4">
-          <RadioGroup className="flex flex-wrap mb-4">
-            {sizes.map((size) => (
-              <div key={size} className="flex items-center space-x-2">
-                <RadioGroupItem value={size} id={size}>
-                  {size}
-                </RadioGroupItem>
-              </div>
-            ))}
-          </RadioGroup>
-        </RadioGroup>
-        <h1>Hola mundo</h1>
-      </main>
+
     </>
   )
 }
